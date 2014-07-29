7fae1c60b46d24e3ccd5c667a993517a69d33483
Alan Christopher <alanchristophercty@gmail.com>
2014-07-28 17:43:10 -0700

    Fixed corrupted check_submission.py.

--- proj2/check_submission.py
+++ proj2/check_submission.py
@@ -22,25 +22,25 @@ def run(command, *args, **keys):
 
 
 def is_in_git():
-	'''Returns true if the working directory is under version control by git.'''
-	start = os.getcwd();
-	while True:
-		if os.path.isdir('.git'):
-			os.chdir(start);
-			return True
-		elif (os.getcwd() == '/'):
-			os.chdir(start);
-			return False
-		else:
-			os.chdir("..");
+    '''Returns true if the working directory is under version control by git.'''
+    start = os.getcwd();
+    while True:
+        if os.path.isdir('.git'):
+            os.chdir(start);
+            return True
+        elif (os.getcwd() == '/'):
+            os.chdir(start);
+            return False
+        else:
+            os.chdir("..");
 
 parser = argparse.ArgumentParser(description='Sanity check project 1 submission.');
-parser.add_argument('--tmp', dest='tmp_dir', metavar='TMP_PATH', default='tmp', 
-					help='The path to a directory which will be used for temporary unpacking of' 
-					+ ' data. This directory should not exist before this script is run, and'
-					+ ' should be specified relative to the current working directory. Default is tmp.');
+parser.add_argument('--tmp', dest='tmp_dir', metavar='TMP_PATH', default='tmp',
+                    help='The path to a directory which will be used for temporary unpacking of'
+                    + ' data. This directory should not exist before this script is run, and'
+                    + ' should be specified relative to the current working directory. Default is tmp.');
 parser.add_argument('--account', dest='acct', metavar="ACCOUNT",
-					help='The name of your account. Defaults to $USER.')
+                    help='The name of your account. Defaults to $USER.')
 
 args = parser.parse_args()
 subm = 'proj2';
@@ -56,9 +56,9 @@ os.mkdir(tmp_dir);
 
 #The student whose repository we're pulling
 if not acct:
-	stud = check_output("echo -n $USER", shell=True)[-2:]
+    stud = check_output("echo -n $USER", shell=True)[-2:]
 else:
-	stud = acct[-2:]
+    stud = acct[-2:]
 
 #Path to the student's repository
 git_path = 'git@github.com:ucberkeley-cs61c/cs61c-%s.git' % stud
@@ -68,10 +68,10 @@ start_dir = os.getcwd();
 os.chdir(tmp_dir);
 #check that we're not about to get ourselves into some git-in-git troubles.
 if is_in_git():
-	os.chdir(start_dir);
-	call(["rm", "-rf", tmp_dir]);
-	sys.exit(tmp_dir + ' is already under version control. Pulling into it would result in git-in-git issues. You can specify a '
-			 + 'different temporary directory for me to use with the --tmp option.');
+    os.chdir(start_dir);
+    call(["rm", "-rf", tmp_dir]);
+    sys.exit(tmp_dir + ' is already under version control. Pulling into it would result in git-in-git issues. You can specify a '
+             + 'different temporary directory for me to use with the --tmp option.');
 
 out = None
 err = None
@@ -81,34 +81,33 @@ code = 0
 run("git init");
 code, out, err = run("git pull %s tags/%s", git_path, subm);
 if code:
-	print "Could not find the tag [%s] in the repository [%s]" % (subm, git_path)
-	os.chdir(start_dir);
-	call(["rm", "-rf", tmp_dir]);
-	sys.exit();
+    print "Could not find the tag [%s] in the repository [%s]" % (subm, git_path)
+    os.chdir(start_dir);
+    call(["rm", "-rf", tmp_dir]);
+    sys.exit();
 
 #Search for the marker file.
 dirs = check_output(["find . -name " + fbf + " -printf \'%h\\n\'"], shell=True).split()
 if len(dirs) == 0:
-	print 'Could not find ' + fbf + '. ' + fbf + ' should only exist in exactly place inside your repository.'
-	sys.exit()
+    print 'Could not find ' + fbf + '. ' + fbf + ' should only exist in exactly place inside your repository.'
+    sys.exit()
 elif len(dirs) > 1:
-	print 'Found ' + fbf + ' in multiple locations. ' + fbf + ' should only exist in one place inside your repository.'
+    print 'Found ' + fbf + ' in multiple locations. ' + fbf + ' should only exist in one place inside your repository.'
 #pull time of commit information.
 os.chdir(dirs[0]);
 
 try:
-	time_stamp = check_output("git log -n 1 --format=fuller | grep CommitDate", shell=True);
-	print "Submission date %s" % time_stamp[time_stamp.find(':'):]
-	if os.path.exists("Makefile"):
-		print "Running Benchmark."
-		print
-		call("make bench-fast", shell=True);
+    time_stamp = check_output("git log -n 1 --format=fuller | grep CommitDate", shell=True);
+    print "Submission date %s" % time_stamp[time_stamp.find(':'):]
+    if os.path.exists("Makefile"):
+        print "Running Benchmark."
+        print
+        call("make bench-fast", shell=True);
         call("./bench-fast")
-		
-	else:
-		print "No Makefile found."
+    else:
+        print "No Makefile found."
 except CalledProcessError:
-	print "No submission found."
+    print "No submission found."
 
 os.chdir(start_dir)
 call(["rm", "-rf", tmp_dir]);
